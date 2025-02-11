7466eed545fec376145306b978b75e06b3a42a1e
Salin Shrestha <salin.shrestha.pro@gmail.com>
2025-02-11 19:46:21 +0545

    version 1

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home() {
           👀 More coming soon. Stay tuned. 
           <br></br>!!!EXPERIMENTAL PAGE!!!
         </p>
-        <p className="text-white-100"> 
+        <p className="text-white text-sm"> 
           &copy; 2025 Salin Shrestha (@tmsalinshrestha). All rights reserved.
         </p>
       </div>
