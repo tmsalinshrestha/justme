cb54709d845d977c9834559f8ed3d2913efa5798
Salin Shrestha <salin.shrestha.pro@gmail.com>
2025-02-12 19:36:31 +0545

    version 1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import { useEffect } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
